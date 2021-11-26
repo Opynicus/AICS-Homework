396ae05020f6e6e80daa018e0188a16fc3d4630e
ZTLPluz <zenguncle@outlook.com>
2021-11-26 20:41:11 +0800

    delete

--- code_chap_4_student/exp_4_4_custom_tensorflow_op_student/run_exp_4_4.py
+++
@@ -1,4 +0,0 @@
-rm *.jpg
-
-./install_stu_tensorflow.sh
-python main_exp_4_4.py
--- code_chap_4_student/exp_4_4_custom_tensorflow_op_student/run_style_transfer.sh
+++
@@ -1,4 +0,0 @@
-rm *.pyc
-rm *.jpg
-
-python stu_upload/transform_cpu.py ./images/chicago.jpg ./models/pb_models/udnie.pb  ./stu_upload/udnie_power_diff.pb ./stu_upload/udnie_power_diff_numpy.pb
